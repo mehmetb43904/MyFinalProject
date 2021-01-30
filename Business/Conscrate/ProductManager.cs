f6417e5b64c59860586e8a7d9c05368689e8ba2e
mehmet bozkurt <mehmetb._239@outlook.com>
2021-01-31 00:38:32 +0300

    Add project files.

---
+++ Business/Conscrate/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrate.InMemory;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Conscrate
+{
+   public  class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //Yetkisi var mı ?
+
+            return _productDal.GetAll();
+        }
+    }
+}
